cb45c28df0c22ab53150bae2a870e112280e4bcb
David Saidov <delightxxls@gmail.com>
2025-05-19 11:30:07 +0300

    chore: generate camel case aliases for currency details

--- src/schemas/currency_rates.py
+++ src/schemas/currency_rates.py
@@ -4,6 +4,8 @@ from string import ascii_letters
 from typing import Annotated
 
 from pydantic import BaseModel, Field, RootModel, field_validator
+from pydantic import ConfigDict
+from pydantic.alias_generators import to_camel
 
 from schemas.base import BaseRequestModel, BaseResponseModel
 
@@ -17,6 +19,11 @@ class CurrencyDetails(BaseModel):
         to_cur (str): The target currency code (ISO 4217 format).
         date (datetime.date): The date of the currency rate.
     """
+    model_config = ConfigDict(
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
+
     from_cur: Annotated[
         str,
         Field(
